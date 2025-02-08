cf7f2f883071418895a2bccac898113db903b7e9
hd <hdshare@vip.qq.com>
2025-02-09 00:18:12 +0800

    feat: luaj/compiler/IntPtr

---
+++ src/main/java/luaj/compiler/IntPtr.java
@@ -0,0 +1,13 @@
+package luaj.compiler;
+
+public class IntPtr {
+    int i;
+
+    public IntPtr() {
+        throw new RuntimeException("Stub!");
+    }
+
+    public IntPtr(int value) {
+        throw new RuntimeException("Stub!");
+    }
+}
