d201a84df245758020064f1e9c4dcaafae558c98
hd <hdshare@vip.qq.com>
2025-02-09 00:18:02 +0800

    feat: luaj/compiler/InstructionPtr

---
+++ src/main/java/luaj/compiler/InstructionPtr.java
@@ -0,0 +1,18 @@
+package luaj.compiler;
+
+public class InstructionPtr {
+    final int[] code;
+    final int idx;
+
+    public InstructionPtr(int[] code, int idx) {
+        throw new RuntimeException("Stub!");
+    }
+
+    public int get() {
+        throw new RuntimeException("Stub!");
+    }
+
+    public void set(int value) {
+        throw new RuntimeException("Stub!");
+    }
+}
