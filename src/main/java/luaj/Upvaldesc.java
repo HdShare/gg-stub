16d71ac9133fdb5f93c77cea1727fd7dcf8052f4
hd <hdshare@vip.qq.com>
2025-02-08 23:53:07 +0800

    feat: luaj/Upvaldesc

---
+++ src/main/java/luaj/Upvaldesc.java
@@ -0,0 +1,15 @@
+package luaj;
+
+public class Upvaldesc {
+    public final short idx;
+    public final boolean instack;
+    public LuaString name;
+
+    public Upvaldesc(LuaString name, boolean instack, int idx) {
+        throw new RuntimeException("Stub!");
+    }
+
+    public String toString() {
+        throw new RuntimeException("Stub!");
+    }
+}
