da79ea5bfafa0584c541ad5a256a51e50336f4e6
hd <hdshare@vip.qq.com>
2025-02-08 23:38:57 +0800

    feat: luaj/LocVars

---
+++ src/main/java/luaj/LocVars.java
@@ -0,0 +1,23 @@
+package luaj;
+
+public class LocVars {
+    public int endpc;
+    public int startpc;
+    public LuaString varname;
+
+    public LocVars(LuaString varname, int startpc, int endpc) {
+        throw new RuntimeException("Stub!");
+    }
+
+    public String tojstring() {
+        throw new RuntimeException("Stub!");
+    }
+
+    public String toString() {
+        throw new RuntimeException("Stub!");
+    }
+
+    public static void main(String[] args) throws Throwable {
+        throw new RuntimeException("Stub!");
+    }
+}
