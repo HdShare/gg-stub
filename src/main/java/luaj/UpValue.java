0d4f44198650f92bab65eec7a256efb35823469e
hd <hdshare@vip.qq.com>
2025-02-08 23:52:40 +0800

    feat: luaj/UpValue

---
+++ src/main/java/luaj/UpValue.java
@@ -0,0 +1,30 @@
+package luaj;
+
+public final class UpValue {
+    LuaValue[] array;
+    int index;
+
+    public UpValue(LuaValue[] stack, int index) {
+        throw new RuntimeException("Stub!");
+    }
+
+    public String toString() {
+        throw new RuntimeException("Stub!");
+    }
+
+    public String tojstring() {
+        throw new RuntimeException("Stub!");
+    }
+
+    public final LuaValue getValue() {
+        throw new RuntimeException("Stub!");
+    }
+
+    public final void setValue(LuaValue value) {
+        throw new RuntimeException("Stub!");
+    }
+
+    public final void close() {
+        throw new RuntimeException("Stub!");
+    }
+}
